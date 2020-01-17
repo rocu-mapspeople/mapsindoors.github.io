6f26795adcf8b8b9f664cb66ac46f91ff36d3d6e
Mark Hougaard Jensen <mark@marks.dk>
2020-01-17 13:55:13 +0100

    gulp: Limit gulp-file to just compiling SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,31 @@
 'use strict';
 
-const browsersync = require("browser-sync").create();
-const cp = require("child_process");
 const del = require("del");
 const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-const newer = require("gulp-newer");
+const { series } = require('gulp');
 const sass = require('gulp-dart-sass');
 
-function browserSync(done) {
-    browsersync.init({
-      server: {
-        baseDir: "./_site/"
-      },
-      port: 3000
-    });
-    done();
+function clean(cb) {
+    return del(["./_site/assets/css/", './assets/css/']);
+    cb;
 }
 
-function browserSyncReload(done) {
-    browsersync.reload();
-    done();
-}
-
-function clean() {
-    return del(["./_site/assets/"]);
-}
-
-function jekyll() {
-    return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
-}
-
-function css() {
+function css(cb) {
     return gulp
         .src("./css/**/*.scss")
         .pipe(sass({ outputStyle: "expanded" }).on('error', sass.logError))
-        .pipe(gulp.dest("./_site/assets/css/"))
-        // .pipe(browsersync.stream());
+        .pipe(gulp.dest("./_site/assets/css/")) // Used for serving the newly changed css-files when previewing the Jekyll site locally
+        .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css/')) // Used for building the css-files for production-use as 'assets' is copied in full when GH-pages builds the site
+        cb;
 }
 
-function images() {
-    return gulp
-      .src("./assets/img/**/*")
-      .pipe(newer("./_site/assets/img"))
-      .pipe(gulp.dest("./_site/assets/img"));
+function watchFiles(cb) {
+    gulp.watch("./css/**/*", css);
+    cb;
 }
 
-function watchFiles() {
-    gulp.watch("./assets/scss/**/*", css);
-    gulp.watch(
-      [
-        "./_includes/**/*",
-        "./_layouts/**/*",
-        "./_collections/**/*"
-      ],
-      gulp.series(jekyll, browserSyncReload)
-    );
-    gulp.watch("./assets/img/**/*", images);
-}
-
-const build = gulp.series(clean, jekyll, css);
-const watch = gulp.parallel(watchFiles, browserSync);
-
-exports.images = images;
-exports.css = css;
-exports.jekyll = jekyll;
-exports.clean = clean;
-exports.build = build;
-exports.watch = watch;
-exports.default = build;
+exports.clean = clean
+exports.default = series(clean, css)
+exports.build = exports.default
+exports.watch = series(clean, css, watchFiles)
